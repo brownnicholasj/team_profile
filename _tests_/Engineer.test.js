f019a88133b9ca9fef3c652eb57872e1da2fb4f0
Nicholas Brown <brown.nicholasj@gmail.com>
2021-05-16 08:08:20 -0500

    continue test creation from engineer -- switch computers

--- _tests_/Engineer.test.js
+++ _tests_/Engineer.test.js
@@ -1,7 +1,14 @@
 const Engineer = require('../lib/engineer');
+const Employee = require('../lib/employee');
+
+jest.mock('../lib/employee');
 
 describe('Engineer class', () => {
-	it('given officeNumber is return from officeNumber key', () => {
-		expect(new Engineer(`5`).officeNumber).toBe(5);
+	it('given github name is returned from github key', () => {
+		const engineer = new Engineer('eName_github');
+
+		const output = engineer.getGithub();
+
+		expect(output).toBe('eName_github');
 	});
 });
