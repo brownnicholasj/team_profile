a65de2ff8b0df44b714df8219d334f4fae60f8a8
Nicholas Brown <brown.nicholasj@gmail.com>
2021-05-16 15:41:40 -0500

    questions are in and repeating properly on prompt

--- index.js
+++ index.js
@@ -2,21 +2,28 @@ const inquirer = require('inquirer');
 const chalk = require('chalk');
 const fs = require('fs');
 
-class CollectInputs {
-	constructor() {
-		k;
-	}
-}
-const collectInputs = async (inputs = []) => {
-	const promptManager = [
+let role = 'Manager';
+
+const userAnswers = async (collected = []) => {
+	const teamQuestions = [
 		{
 			type: 'input',
-			message: `What is the team manager's name?`,
-			name: 'managerName',
+			message: `What is the Manager's name?`,
+			name: 'name',
+			when: role === 'Manager',
 		},
-	];
-
-	const promptEmployee = [
+		{
+			type: 'input',
+			message: `What is the Engineers's name?`,
+			name: 'name',
+			when: role === 'Engineer',
+		},
+		{
+			type: 'input',
+			message: `What is the Intern's name?`,
+			name: 'name',
+			when: role === 'Intern',
+		},
 		{
 			type: 'input',
 			message: 'What is their employee ID?',
@@ -27,18 +34,53 @@ const collectInputs = async (inputs = []) => {
 			message: 'What is their email address?',
 			name: 'email',
 		},
+		{
+			type: 'input',
+			message: `What is the Manager's Office Number`,
+			name: 'officeNumber',
+			when: role === 'Manager',
+		},
+		{
+			type: 'input',
+			message: `What is the Engineer's GitHub user name?`,
+			name: 'github',
+			when: role === 'Engineer',
+		},
+		{
+			type: 'input',
+			message: `What is the Intern's school?`,
+			name: 'school',
+			when: role === 'Intern',
+		},
+		{
+			type: 'list',
+			message: 'Do you have another employee to enter?',
+			choices: ['Engineer', 'Intern', 'No'],
+			name: 'newEmployee',
+		},
 	];
 
-	const { again, ...answers } = await inquirer.prompt(prompts);
+	const { newEmployee, ...answers } = await inquirer.prompt(teamQuestions);
 
-	const teamMembers = [...inputs, answers];
+	switch (role) {
+		case 'Manager':
+			answers.role = role;
+		case 'Engineer':
+			answers.role = role;
+		case 'Intern':
+			answers.role = role;
+	}
 
-	return again ? collectInputs(teamMembers) : teamMembers;
+	const employees = [...collected, answers];
+
+	role = newEmployee;
+
+	return newEmployee !== 'No' ? userAnswers(employees) : employees;
 };
 
 const init = async () => {
-	const teamMembers = await collectInputs();
-	console.log(teamMembers);
+	const employees = await userAnswers();
+	console.log(employees);
 	// .then((answers) => writeFileAsync('README.md', generateMarkdown(answers)))
 	// .then(() => console.log('Successfully wrote to README.md'))
 	// .catch((err) => console.error(err));
