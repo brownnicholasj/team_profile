3bd45f8a65aba72166a821f39730e1c9ecdf0279
Nicholas Brown <brown.nicholasj@gmail.com>
2021-05-16 10:32:27 -0500

    completed all tests for engineering

--- _tests_/Engineer.test.js
+++ _tests_/Engineer.test.js
@@ -1,14 +1,24 @@
 const Engineer = require('../lib/engineer');
 const Employee = require('../lib/employee');
 
-jest.mock('../lib/employee');
+const engineer = new Engineer('eName', '3', 'eName@email.com', 'eName_github');
 
 describe('Engineer class', () => {
 	it('given github name is returned from github key', () => {
-		const engineer = new Engineer('eName_github');
-
-		const output = engineer.getGithub();
+		const output = engineer.github;
 
 		expect(output).toBe('eName_github');
+	});
+
+	it('getGithub() returns github url', () => {
+		const output = engineer.getGithub();
+
+		expect(output).toBe('https://www.github.com/eName_github');
+	});
+
+	it('getRole() at the Engineer class overrides Employee and returns "Engineer"', () => {
+		const output = engineer.getRole();
+
+		expect(output).toBe('Engineer');
 	});
 });
